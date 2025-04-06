48bbc5376a91c41b04b8ea6b42c20fdba701e613
PleizzChoi <hello.pleizz@gmail.com>
2025-04-06 18:37:33 +0900

    assignment01: rsp

--- week04/assignment01_rsp.cpp
+++ week04/assignment01_rsp.cpp
@@ -33,14 +33,80 @@ char getComputerChoice()
     return 'W';
 }
 
+void getUserInputWithValidation(string message, char *userChoice)
+{
+    do
+    {
+        cout << message << "O(묵), V(찌), W(빠) 중 하나를 입력하세요: " << endl;
+        cin >> *userChoice;
+        *userChoice = toupper(*userChoice);
+    } while (*userChoice != 'O' && *userChoice != 'V' && *userChoice != 'W');
+}
+
+string toString(char choice)
+{
+    switch (choice)
+    {
+    case 'O':
+        return "묵";
+    case 'V':
+        return "찌";
+    case 'W':
+        return "빠";
+    default:
+        return "*";
+    }
+}
+
+void printStatus(char userChoice, char compChoice)
+{
+    cout << "사용자: " << toString(userChoice) << ", 컴퓨터: " << toString(compChoice) << endl;
+}
+
 int main()
 {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    cout << "첫 번째 라운드(가위바위보 역할) - O(묵), V(찌), W(빠) 중 하나를 입력하세요: ";
+    char userChoice, compChoice;
+    int prevStatus = 0;
 
-    char userChoice;
-    cin >> userChoice;
+    do
+    {
+        getUserInputWithValidation("첫 번째 라운드(가위바위보 역할) - ", &userChoice);
+
+        compChoice = getComputerChoice();
+        prevStatus = rpsResult(userChoice, compChoice);
+
+        printStatus(userChoice, compChoice);
+
+        if (prevStatus == 1)
+            cout << "사용자가 첫 공격" << endl;
+        else if (prevStatus == -1)
+            cout << "컴퓨터가 첫 공격" << endl;
+        else
+            cout << "사용자와 컴퓨터가 비겼으니 다시" << endl;
+
+    } while (prevStatus == 0);
+
+    bool isUserTurn = prevStatus == 1;
+    int rsp = -1;
+
+    while (rsp != 0)
+    {
+        string msg = "사용자의";
+        msg.append((isUserTurn ? "공격" : "수비"));
+        msg.append(" - ");
+
+        getUserInputWithValidation(msg, &userChoice);
+
+        compChoice = getComputerChoice();
+        printStatus(userChoice, compChoice);
+
+        if ((rsp = rpsResult(userChoice, compChoice)) == 0)
+            cout << (isUserTurn ? "사용자" : "컴퓨터") << "승리!" << endl;
+        else if (isUserTurn != (isUserTurn = rsp == 1))
+            cout << "** 공격권이 변경됐어요 **" << endl;
+    }
 
     return 0;
 }
